0117948e9d7d05bfda01bc960337fbe1dc8255ad
dhavalantala <dhavalantala>
2023-10-24 20:55:15 +0200

    Training Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 if __name__ == '__main__':
     try:
@@ -12,14 +13,22 @@ if __name__ == '__main__':
         logger.exception(e)
         raise e
 
-
-
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>>> stage Prepare based model stage started <<<<<<<<<")
         prepare_base_model = PrepareBaseModelTrainingPipeline()
         prepare_base_model.main()
         logger.info(f">>>>>>> stage Prepare based model stage completed <<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>> stage training  started <<<<<<<<<")
+        training = ModelTrainingPipeline()
+        training.main()
+        logger.info(f">>>>>>> stage training completed <<<<<<<<")
     except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,12 +1,17 @@
-import os
 from cnnClassifier.constants import *
+import os
+from pathlib import Path
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, 
-                                                PrepareBaseModelConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
+
+
 
 class ConfigurationManager:
-    def __init__( 
-        self, 
+    def __init__(
+        self,
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
 
@@ -15,6 +20,8 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
+    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -28,7 +35,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-
+    
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
@@ -62,3 +69,29 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+
+      
